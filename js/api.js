9ae003588eb3cf5a23404b37cc3368864d414c04
sialbul <sbaslamisli@yahoo.com>
2021-10-06 12:16:34 -0400

    fixed bugs with api

--- js/api.js
+++ js/api.js
@@ -2,12 +2,11 @@ const apiKey = 'x7Gip5NstAUT2IRcLBkGgWGmHHSUGKwVr0hzJpCw';
 const apiKey2 ="bea611428821d3ac08d2f293893ff247";
 
 async function fetchData() {
-    const stateCode = document.getElementById("state").value;
-    var x = document.querySelector("#state").selectedIndex;
-    const nameCity = document.getElementById("state")[x].innerText;
-console.log(nameCity);
+    const stateCode = document.getElementById("state").value; 
+    const nameCity = document.querySelector(`[value=${stateCode}]`).getAttribute('data-name');
+       
     const url = 'https://developer.nps.gov/api/v1/parks?stateCode=' + stateCode + '&limit=12&api_key=';
-    const urlWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + nameCity + '&units=metric&appid=';
+    const urlWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + nameCity + '&units=imperial&appid=';
     try {
         const response = await fetch(url + apiKey);
         const data = await response.json();
@@ -19,7 +18,6 @@ console.log(nameCity);
     catch (error) {
         console.log(error)
     }
-   
 }
 
 fetchData();
--- js/index.js
+++ js/index.js
@@ -1,28 +1,41 @@
 const convertGPS = (deg, dir) => Math.abs(parseFloat(deg)).toFixed(3) + ' ' + (dir === 'latitude' ? deg >= 0 ? 'N' : 'S' : deg >= 0 ? 'E' : 'W');
-document.getElementById("state").innerHTML = stateList.map(state => `<option value="${state.value}">${state.name}</option>`).join('');
+document.getElementById("state").innerHTML = stateList.map(state => `<option data-name="${state.name}" value="${state.value}" ${state.value === 'ca' && 'selected=selected'}>${state.name}</option>`).join('');
 
 function renderData(data) {
     if (!data) return;
 
     let mappedDiv = '';
     data.data.map(park => {
-        console.log(data)
-
         mappedDiv +=
             `<div class="card">
-            <div>
-                <img class="image" src="${park.images[0].url}" alt="${park.images[0].altText}" />
-            </div>
-            <h2>${park.fullName}</h2>
-            <p class=description>${park.description}</p>
-            <hr/>
-            <div id="activity">
-                <h3>Activities:</h3>
-                <p>${park.activities.slice(0, 3).map(a => a.name).join(', ')}</p>
+                <div>
+                    <img class="image" src="${park.images[0].url}" alt="${park.images[0].altText}" />
+                </div>
+                <h2>${park.fullName}</h2>
+                <p class=description>${park.description}</p>
                 <hr/>
-                <h3>Location:</h3> <p>${convertGPS(park.latitude, 'latitude')} ${convertGPS(park.longitude, 'longitude')}</p>
-                <hr/>
-                <h3 id="linkPage"><a href=${park.url} target="_blank">More info...</a></h3>
+                <div id="activity">
+                    <h3>
+                        <i class="fa fa-tree" aria-hidden="true"></i>
+                        Activities
+                    </h3>
+                    <p>
+                        ${park.activities.slice(0, 3).map(a => a.name).join(', ')}
+                    </p>
+                    <hr/>
+                    <h3>
+                        <i class="fa fa-map-pin" aria-hidden="true"></i>
+                        Location
+                    </h3> 
+                    <p>
+                        ${convertGPS(park.latitude, 'latitude')} ${convertGPS(park.longitude, 'longitude')}
+                    </p>
+                    <hr/>
+                    <h3 id="linkPage">
+                        <a href=${park.url} target="_blank"> 
+                            <i class="fa fa-info-circle" aria-hidden="true"></i> More info...
+                    </a>
+                </h3>
             </div>
         </div>`;
     });
@@ -32,19 +45,22 @@ renderData();
 
 function renderWeatherData(dataWeather) {
     if (!dataWeather) return;
-    const { name } = dataWeather;
-    const { icon, description } = dataWeather.weather[0];
-    const { temp, humidity } = dataWeather.main;
-    const { speed } = dataWeather.wind;
-    console.log(icon)
-    document.getElementById('nameState').innerText = "Welcome to " + name + " parks! Today`s weather";
-    document.getElementById("iconDiv").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.getElementById("description").innerText = description;
-    document.getElementById("description").innerText = description;
-    document.getElementById("humidity").innerText = "Humidity: " + humidity;
-    document.getElementById("wind").innerText = "Speed: " + speed;
-    document.getElementById("arrow").innerText = "Scroll down to see the parks U+02193";
 
+    let mappedDivWeather= `
+    <div class="weatherCard">
+        <h1 id="nameState">Welcome to <br/><span id="nameBig">${dataWeather.name}</span> parks!</h1>
+        <div class="weatherSec">
+            <h3> Today's weather</h3>
+            <img id="iconDiv" src="https://openweathermap.org/img/wn/${dataWeather.weather[0].icon}.png"></span>
+            <h3 id="description">${dataWeather.weather[0].description}</h3>
+                <div class="symbols">
+                    <h3 id="temp"><i class="fas fa-thermometer-three-quarters"></i> ${dataWeather.main.temp} °F</h3>
+                    <h3 id="humidity"><i class="fas fa-tint"></i> ${dataWeather.main.humidity}</h3>
+                    <h3 id="wind"><i class="fas fa-wind"></i> ${dataWeather.wind.speed}</h3>
+                </div>
+        </div>
+    </div>   
+    `;
+    document.getElementById('hero').innerHTML = mappedDivWeather;
 }
-
 renderWeatherData();
